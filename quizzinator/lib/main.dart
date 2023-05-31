9c48f54ec1b2fc670ed939a12f245499049aefc0
Michael Mboya <mikemboya2@gmail.com>
2023-05-31 08:37:09 +0530

    Set up supabase

--- quizzinator/lib/main.dart
+++ quizzinator/lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
-  runApp(const MainApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://sautaxawazmyacdnjbbh.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNhdXRheGF3YXpteWFjZG5qYmJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODIzNDc2NDYsImV4cCI6MTk5NzkyMzY0Nn0.2bI5eEI06wC05nZ6zZzUUtbUVkFDHs5JjaU91EKRB2Q',
+  );
+
+  runApp(const ProviderScope(child: MainApp()));
 }
 
 class MainApp extends StatelessWidget {
